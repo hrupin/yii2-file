1e5e62e3401d17843aa2b0ed6826f93051001ce8
sandro <sandro@debian.sandro>
2016-01-09 18:31:18 +0300

    Rename main class

--- File.php
+++ File.php
@@ -5,7 +5,7 @@ namespace hrupin\file;
 /**
  * This is just an example.
  */
-class AutoloadExample extends \yii\base\Widget
+class File extends \yii\base\Widget
 {
     public function run()
     {
